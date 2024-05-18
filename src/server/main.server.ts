5933ef9175b0c71504181f43ccde1fa73987909a
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2024-05-18 11:33:45 -0500

    Weave without fusion setup

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,12 +1,1 @@
-import { Players, TweenService } from "@rbxts/services";
-import Weave from "shared/Weave";
-
-// const players = Weave.WeaveObject<Record<string, boolean>>("PlayerLeves", {});
-
-// Players.PlayerAdded.Connect((player: Player) => {
-// 	const p = players.get();
-// 	p[`${player.UserId}`] = true;
-// 	players.set(p);
-// });
-
-print("here i amasdf server");
+print("print from the server");
